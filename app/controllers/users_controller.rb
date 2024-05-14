99d4019396750aa605efb57ff5adb65545c0c589
CpedniyNikon <carry2002life@mail.ru>
2024-05-14 17:37:37 +0300

    dev(app): added validation and edit/delete user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,46 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
-    if user_params.values.any?(&:blank?)
-      flash[:alert] = 'Please fill in all fields'
-      redirect_to new_user_path
-    else
-      User.create(user_params)
+    @user = User.new(user_params)
+
+    if @user.save
       redirect_to root_path, notice: 'You successfully registered'
+    else
+      flash.now[:alert] = 'Please fill in all fields'
+      render :new
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'user data is changed'
+    else
+      flash.now[:alert] = 'user cant be changed'
+
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    # TODO
+    # session.delete(:user_id)
+
+    redirect_to root_path, notice: 'user deleted'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :nickname, :email,
+    :password, :password_confirmation)
   end
 end
